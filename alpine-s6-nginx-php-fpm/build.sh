cc1986f82a6648b5e86b4da44533004fb20f9795
moonbuggy <3319867+moonbuggy@users.noreply.github.com>
2021-03-08 23:47:03 +1100

    improved multi-arch

--- alpine-s6-nginx-php-fpm/build.sh
+++ alpine-s6-nginx-php-fpm/build.sh
@@ -1,9 +1,11 @@
 #! /bin/bash
 
-NOOP='true'
+#NOOP='true'
+NOPUSH='true'
+#NOBUILD='true'
 
 DOCKER_REPO="${DOCKER_REPO:-moonbuggy2000/alpine-s6-nginx-php-fpm}"
-DOCKER_TAG="${1:-7.3}"
+DOCKER_TAG="${1:-latest}"
 IMAGE_NAME="${DOCKER_REPO}:${DOCKER_TAG}"
 
 . "../.common/build.sh"
--- alpine-s6-nginx/build.sh
+++ alpine-s6-nginx/build.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 
-NOOP='true'
+#NOOP='true'
+NOPUSH='true'
+#NOBUILD='true'
 
 DOCKER_REPO="${DOCKER_REPO:-moonbuggy2000/alpine-s6-nginx}"
 DOCKER_TAG="${1:-1.18}"
