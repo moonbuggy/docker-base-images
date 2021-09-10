f70a69d2edab77d515a5a03de007bd92920380dc
moonbuggy <3319867+moonbuggy@users.noreply.github.com>
2021-09-10 23:35:26 +1000

    reworked nuitka/build.conf

--- nuitka/build.sh
+++ nuitka/build.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
+# shellcheck disable=SC2034
 
 #NOOP='true'
 #DO_PUSH='true'
@@ -6,7 +7,7 @@
 
 DOCKER_REPO="${DOCKER_REPO:-moonbuggy2000/nuitka}"
 
-all_tags='3.7-alpine 3.7-debian 3.8-alpine 3.8-debian'
+all_tags='3.7-alpine 3.7-debian 3.8-alpine 3.8-debian 3.9-alpine 3.9-debian'
 default_tag='latest'
 
 . "hooks/.build.sh"
