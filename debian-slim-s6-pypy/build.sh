e469040c90e1f8fc01c33be12945ab6815546e33
moonbuggy <3319867+moonbuggy@users.noreply.github.com>
2023-05-22 12:43:10 +1000

    added check/update to debian-slim-s6-python; version bump for debian-slim-s6-pypy

--- debian-slim-s6-pypy/build.sh
+++ debian-slim-s6-pypy/build.sh
@@ -8,6 +8,6 @@
 DOCKER_REPO="${DOCKER_REPO:-moonbuggy2000/debian-slim-s6-pypy}"
 
 all_tags='2.7 3.5 3.6 3.7 3.8 3.9'
-default_tag='3.8'
+default_tag='3.9'
 
 . "hooks/.build.sh"
--- debian-slim-s6-python/build.sh
+++ debian-slim-s6-python/build.sh
@@ -11,4 +11,15 @@ DOCKER_REPO="${DOCKER_REPO:-moonbuggy2000/debian-slim-s6-python}"
 all_tags='2.7 3.3 3.4 3.5 3.6 3.7 3.8 3.9 3.10'
 default_tag='latest'
 
+# version 3.2 doesn't build
+MINIMUM_VERSION='3.3'
+
+# TARGET_VERSION_TYPE='minor'
+
+# version 3.12 doesn't build
+TARGET_VERSION_TYPE='custom'
+custom_versions () {
+  echo "$(parse_version_minor "$(echo ${SOURCE_MINOR_VERSIONS} | xargs -n1 | grep -vP '^3\.12.*' | xargs)")"
+}
+
 . "hooks/.build.sh"
