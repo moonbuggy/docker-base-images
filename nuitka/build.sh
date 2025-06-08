98d99f1e6de04bb4162c2b4092602791cb66ad27
moonbuggy <3319867+moonbuggy@users.noreply.github.com>
2025-06-09 00:05:32 +1000

    multi-arch Nuitka

--- nuitka/build.sh
+++ nuitka/build.sh
@@ -7,7 +7,22 @@
 
 DOCKER_REPO="${DOCKER_REPO:-moonbuggy2000/nuitka}"
 
-python_versions='3.8 3.9 3.10 3.11 3.12'
+# debian stopped building in 3.8 and 3.9 on some architectures,
+# can't be bothered making exceptions in build.conf
+# python_versions='3.8 3.9 3.10 3.11 3.12 3.13'
+python_versions='3.10 3.11 3.12 3.13'
+
+# this breaks because it doesn't handle the tag processing below, it's easier
+# just to do './build.sh all' and make the latest version
+# TARGET_VERSION_TYPE='major'
+#
+# custom_source_versions() {
+#   echo "$(git_repo_tags 'nuitka/nuitka')"
+# }
+#
+# custom_target_versions() {
+#   echo ${REPO_TAGS} | xargs -n1 | grep -oE '([0-9]*\.){2}[0-9\.]*(?=-py)*' | sort -uV
+# }
 
 all_alpine=""
 all_debian=""
@@ -19,6 +34,7 @@ done
 all_tags="${all_alpine} ${all_debian}"
 default_tag='latest'
 
+# build for all specified python verisons by default
 tags=''
 for tag in "${@}"; do
   [ "x${tag}" = "x${tag//pyall/}" ] \
